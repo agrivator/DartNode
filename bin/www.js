28ed217e7629660e399ca8d4112a36d156a3e974
Jyothis P <jyothisp52@gmail.com>
2020-05-14 01:53:53 +0530

    Connects to db now.

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,12 @@
 const app = require("../index");
-
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
+
+
+mongoose.connect("mongodb://localhost:27017/dart", {useNewUrlParser: true,  useUnifiedTopology: true})
+    .then(() => console.log("Connected to MongoDB."))
+    .catch(err => console.error("Could not connect to MongoDB."));
 
 
 app.listen(PORT, function () {
