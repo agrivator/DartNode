53a34bc9f5838065e77cf00a3e7c41dd782b4ad0
akhilmhdh <akhilmhdh@gmail.com>
2020-05-15 14:46:53 +0530

    =added db

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,3 @@ const db = process.env.MONGODB_URL || "mongodb://localhost:27017/dart";
 mongoose.connect(db, {useNewUrlParser: true,  useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB."))
     .catch(err => console.error("Could not connect to MongoDB."));
-
-mongoose.connect()
-
-export default mongoose;
